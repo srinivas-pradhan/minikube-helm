0c2eddf4bd0f07733e40aec71ab80f2cc884ef4f
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-02-27 06:42:00 -0500

    Add env vars for helm magic

--- projects/dummy-app/app.py
+++ projects/dummy-app/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+    print(os.getenv('MY_CONF_VAR'))
+    print(os.getenv('MY_SECRET_VAR'))
 
 if __name__ == "__main__":
     app.run()
+
